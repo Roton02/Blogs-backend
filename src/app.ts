b3acea946e7305abf481babb23afe50dc4a4310f
Roton02 <rph645102@gmail.com>
2025-01-18 21:41:20 +0600

    Add Page i first api

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Application } from 'express'
+import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
 import router from './app/router'
 import globalErrorHandler from './app/middleware/globalErrorHandler'
@@ -9,6 +9,10 @@ const app: Application = express()
 app.use(express.json())
 app.use(cors())
 app.use('/api', router)
+
+app.use('/', (req:Request, res:Response) => {
+    res.send('Hello World!')
+})
 
 app.use(globalErrorHandler)
 app.use(notFound)
